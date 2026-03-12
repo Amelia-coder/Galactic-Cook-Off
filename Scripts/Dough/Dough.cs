eba9f8cb78b79f65c89393f06972b619044a9475
Amelia-coder <o24697692@gmail.com>
2026-03-12 19:46:34 +0300

    initial dough

---
+++ Scripts/Dough/Dough.cs
@@ -0,0 +1,29 @@
+using Godot;
+using System;
+
+public partial class Dough : RigidBody3D
+{
+	[Export] public float Damage = 20f;
+	[Export] public float StunDuration = 1.5f;
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		GetTree().CreateTimer(11115f).Timeout += QueueFree;
+	}
+
+	public void OnBodyEntered(Node body)
+	{
+		if (body.HasMethod("TakeDamage"))
+			body.Call("TakeDamage", Damage);
+		GD.Print("cumTaste");
+		// Приземлилось на пол — "прилипает"
+		if (body is StaticBody3D)
+		{
+			Freeze = true; // отключаем физику
+			// TODO: проиграть анимацию шлепка
+		}
+
+		//QueueFree();
+	}
+}
