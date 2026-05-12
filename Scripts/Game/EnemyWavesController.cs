dfb493773f5fe691fea574994154f2b6824d2cf9
Amelia-coder <o24697692@gmail.com>
2026-05-12 19:31:47 +0300

    Adds basics for waves

---
+++ Scripts/Game/EnemyWavesController.cs
@@ -0,0 +1,38 @@
+﻿using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scripts.Game
+{
+    public partial class EnemyWavesController
+    {
+        //[Export] public PackedScene EnemyScene;
+
+        //public async Task RunWave(WaveDefinition wave)
+        //{
+        //    for (int i = 0; i < wave.EnemyCount; i++)
+        //    {
+        //        SpawnEnemy(wave);
+
+        //        await ToSignal(
+        //            GetTree().CreateTimer(wave.SpawnInterval),
+        //            "timeout");
+        //    }
+        //}
+
+        //private void SpawnEnemy(WaveDefinition wave)
+        //{
+        //    var enemy = EnemyScene.Instantiate<Node3D>();
+
+        //    AddChild(enemy);
+
+        //    enemy.GlobalPosition = GetRandomSpawnPoint();
+
+        //    // later:
+        //    // enemy.Health *= wave.HealthMultiplier;
+        //}
+    }
+}
